25e0ad8c6c3436c45a5356a208b2f98eaef5df85
Simon Foster <funkysi1701@gmail.com>
2019-10-13 23:12:13 +0100

    format buttons

--- PwnedPass2/ViewModels/ItemsViewModel.cs
+++ PwnedPass2/ViewModels/ItemsViewModel.cs
@@ -14,12 +14,67 @@ namespace PwnedPass2.ViewModels
     {
         public ObservableCollection<HIBPModel> Items { get; set; }
         public Command LoadItemsCommand { get; set; }
+        public string DateSort { get; set; }
+        public string NameSort { get; set; }
+        public string CountSort { get; set; }
 
         public ItemsViewModel(bool order, string orderby)
         {
             Title = "';** pwned pass";
             Items = new ObservableCollection<HIBPModel>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand(order, orderby));
+            SetSort(order, orderby);
+            SetSort(order, orderby);
+            SetSort(order, orderby);
+        }
+
+        private void SetSort(bool order, string orderby)
+        {
+            if (orderby == "AddedDate")
+            {
+                if (order)
+                {
+                    DateSort = "^ Date";
+                    NameSort = "Name";
+                    CountSort = "Pwned Acc";
+                }
+                else
+                {
+                    DateSort = "v Date";
+                    NameSort = "Name";
+                    CountSort = "Pwned Acc";
+                }
+            }
+            if (orderby == "Name")
+            {
+                if (order)
+                {
+                    DateSort = "Date";
+                    NameSort = "^ Name";
+                    CountSort = "Pwned Acc";
+                }
+                else
+                {
+                    DateSort = "Date";
+                    NameSort = "v Name";
+                    CountSort = "Pwned Acc";
+                }
+            }
+            if (orderby == "PwnCount")
+            {
+                if (order)
+                {
+                    DateSort = "Date";
+                    NameSort = "Name";
+                    CountSort = "^ Pwned Acc";
+                }
+                else
+                {
+                    DateSort = "Date";
+                    NameSort = "Name";
+                    CountSort = "v Pwned Acc";
+                }
+            }
         }
 
         private async Task ExecuteLoadItemsCommand(bool sortdir, string orderby)
