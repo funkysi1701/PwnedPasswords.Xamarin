d70124fc9e5c34374464bae7a56d9778942b0234
Simon Foster <funkysi1701@gmail.com>
2022-04-08 09:34:32 +0000

    Merged PR 1170: move core
    move core

---
+++ PwnedPasswords.Core/HIBP.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+using System;
+
+namespace PwnedPasswords.Core
+{
+    public class HIBP
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Name { get; set; }
+
+        public string Domain { get; set; }
+
+        public DateTime BreachDate { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public int PwnCount { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public bool IsFabricated { get; set; }
+
+        public bool IsSensitive { get; set; }
+
+        public bool IsRetired { get; set; }
+
+        public bool IsSpamList { get; set; }
+
+        public string LogoType { get; set; }
+
+        public string LogoPath { get; set; }
+    }
+}
